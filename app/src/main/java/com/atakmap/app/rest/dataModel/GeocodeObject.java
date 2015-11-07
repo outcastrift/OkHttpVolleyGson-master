cfca039468618537852f598b8671ac758d61adb4
outcastrift <outcastrift@gmail.com>
2015-11-07 15:17:08 -0500

    Refactored everything to the name it should be using.

---
+++ app/src/main/java/com/atakmap/app/rest/dataModel/GeocodeObject.java
@@ -0,0 +1,30 @@
+package com.atakmap.app.rest.dataModel;
+
+/**
+ * Created by Sam on 07-Nov-15.
+ */
+public class GeocodeObject
+    {
+        private String mTitle;
+        private String mBody;
+
+    public String getTitle()
+    {
+        return mTitle;
+    }
+
+    public void setTitle(String title)
+    {
+        mTitle = title;
+    }
+
+    public String getBody()
+    {
+        return mBody;
+    }
+
+    public void setBody(String body)
+    {
+        mBody = body;
+    }
+}
